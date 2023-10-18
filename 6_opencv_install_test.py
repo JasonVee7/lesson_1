dd7ee3af1bdc0cd3612a1ba38e55eeb60c82ea27
JasonVee7 <125880073+JasonVee7@users.noreply.github.com>
2023-10-18 13:51:56 +1000

    Add files via upload

--- 6_opencv_install_test.py
+++ 6_opencv_install_test.py
@@ -3,4 +3,10 @@ try:
     print("OpenCV is installed.")
     print("OpenCV version:", cv2.__version__)
 except ImportError:
-    print("OpenCV is not installed.")
+    install_opencv = input("OpenCV is not installed. Do you want to install it? (y/n): ").strip().lower()
+    if install_opencv == 'y':
+        import subprocess
+        subprocess.call(['pip', 'install', 'opencv-python'])
+        print("OpenCV has been successfully installed.")
+    else:
+        print("OpenCV was not installed.")
