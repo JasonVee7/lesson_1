b9c1fb28d6dc1662fbb319dc7c8a288bcf2e8803
JasonVee7 <125880073+JasonVee7@users.noreply.github.com>
2023-10-18 13:47:16 +1000

    Add files via upload

---
+++ 5_date_time.py
@@ -0,0 +1,8 @@
+import datetime
+
+# Get the current date and time
+current_datetime = datetime.datetime.now()
+
+# Format and display the current date and time
+formatted_datetime = current_datetime.strftime("%d-%m-%Y %H:%M:%S")
+print("Current Date and Time:", formatted_datetime)
