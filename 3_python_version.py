cfc7973f5f606543aa6b23bf3a45e0b8c8e5e72b
JasonVee7 <125880073+JasonVee7@users.noreply.github.com>
2023-11-26 13:35:46 +1000

    Update 3_python_version.py

--- 3_python_version.py
+++ 3_python_version.py
@@ -2,3 +2,9 @@ import sys
 
 print("Python Version Checker")
 print("You are currently using Python", sys.version.split()[0])
+
+
+print("Installed Python versions:")
+for path in sys.path:
+    if "python" in path.lower():
+        print(path)
