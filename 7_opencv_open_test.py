43ab99b3271fdf3f433f0ed09e6b6b5ce42cb5a3
JasonVee7 <125880073+JasonVee7@users.noreply.github.com>
2023-11-26 13:42:55 +1000

    Update 7_opencv_open_test.py

--- 7_opencv_open_test.py
+++ 7_opencv_open_test.py
@@ -3,11 +3,21 @@ import cv2
 # Create a frame window
 cv2.namedWindow('OpenCV Window', cv2.WINDOW_NORMAL)
 
-# Wait for the window to be closed
+# Access the webcam (change the index if you have multiple webcams)
+video_capture = cv2.VideoCapture(0)
+
 while True:
+    # Capture frame-by-frame from the webcam
+    ret, frame = video_capture.read()
+
+    # Display the captured frame in the window
+    cv2.imshow('OpenCV Window', frame)
+
+    # Check for the 'Esc' key to exit
     key = cv2.waitKey(1)
     if key == 27:  # 27 is the ASCII code for the 'Esc' key
         break
 
-# Close the window
+# Release the webcam and close the window
+video_capture.release()
 cv2.destroyAllWindows()
